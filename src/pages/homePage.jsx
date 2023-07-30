d214619c4dc6420401c48be57bd5ea7139bdf560
RahulGorai0202 <rahulgorai0206@gmail.com>
2023-07-30 10:52:04 +0530

    Poster slider and entertainment

--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -5,7 +5,7 @@ import DefaultLayoutHOC from "../layouts/defaultLayout";
 
 // Components
 import EntertainmentComponent from "../components/entertainment/entertainmentComponent";
-import heroCarousalComponent from "../components/horeCarousal/heroCarousalComponent";
+import HeroCarousalComponent from "../components/horeCarousal/heroCarousalComponent";
 import PosterSlider from "../components/posterSlider/posterSlider";
 
 function HomePage() {
@@ -13,7 +13,23 @@ function HomePage() {
     const [premireMovies, setPremireMovies] =useState([]);
     const [onlineStreamEvents,setOnlineStreamEvents] = useState([]);
 
-    return <div>Hiii</div>
+    return (
+        <>
+        <HeroCarousalComponent/>
+        <div className="container mx-auto px-4 md:px-12 my-8">
+            <h1 className="text-2xl font-bold text-gray-800 sm:ml-3 ml-0 my-3">The Best of Entertainment</h1>
+            <EntertainmentComponent/>
+        </div>
+        <div className="container mx-auto px-4 md:px-12 my-8">
+            <PosterSlider
+            title="Recomemded Movies"
+            subject="List of recomended movies"
+            posters={recomendedMovies}
+            isDark={false}
+            />
+        </div>
+        </>
+    )
 };
 
 export default DefaultLayoutHOC(HomePage);
