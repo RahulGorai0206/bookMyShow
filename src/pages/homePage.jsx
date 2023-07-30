f716d5da2a7e9ccdc7c838e8c7a4d36792425d4f
RahulGorai0202 <rahulgorai0206@gmail.com>
2023-07-30 13:06:41 +0530

    Main page 3 section done for now

--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -28,6 +28,27 @@ function HomePage() {
             isDark={false}
             />
         </div>
+        <div className="bg-premire-800 py-12">
+            <div className="container mx-auto px-4 md:px-12 my-8 flex flex-col gap-3">
+                <div className="hidden md:flex">
+                    <img src="https://in.bmscdn.com/discovery-catalog/collections/tr:w-1440,h-120/premiere-rupay-banner-web-collection-202104230555.png" alt="Rupay" className="w-full h-full"/>
+                </div>
+                <PosterSlider
+                    title="Premires"
+                    subject="New release every Friday"
+                    posters={premireMovies}
+                    isDark={true}
+                />
+            </div>
+        </div>
+        <div className="container mx-auto px-4 md:px-12 my-8">
+        <PosterSlider
+                    title="Online Streaming Event"
+                    subject=""
+                    posters={onlineStreamEvents}
+                    isDark={false}
+                />
+        </div>
         </>
     )
 };
